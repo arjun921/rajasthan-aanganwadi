18859e0a935010fcde4e9f52f2d137be8e77adb0
arjoonn sharma <arjoonn.94@gmail.com>
2017-09-21 07:26:48 +0000

    Login urls API formulation complete

---
+++ server/server.py
@@ -0,0 +1,72 @@
+import bottle
+import utils
+__version__ = (0, 0, 1)
+
+
+app = bottle.Bottle()
+
+# Generic functions #######################################
+
+
+def httpraise(no, msg):
+    'raise an http error in the respnose'
+    bottle.response.status = no
+    return msg
+
+
+@bottle.route('/<:re:.*>', method='OPTIONS')
+def enableCORSGenericRoute():
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+
+# Route functions #########################################
+
+
+@app.post('/user/login')
+def login():
+    """
+    Expects JSON
+        {
+            'email' : <username>,
+            'pwd'   : <password hash>,
+            'token' : <a 50 character randomly generated token>
+        }
+    Returns OK in case of successful login
+    """
+    status, msg, json = utils.is_valid_login_request(bottle.request)
+    if status == 200:
+        utils.login_user(json)
+    return httpraise(status, msg)
+
+
+@app.post('/user/logout')
+def logout():
+    """
+    Expects JSON
+        {
+            'token' : <token which needs to be logged out>
+        }
+    Returns OK in case of successful login
+    """
+    status, msg, json = utils.is_valid_logout_request(bottle.request)
+    if status == 200:
+        utils.logout_user(json)
+    return httpraise(status, msg)
+
+
+@app.post('/user/create')
+def create_user():
+    """
+    Expects JSON
+        {
+            'name'      : <identifier>
+            'address'   : <address>
+            'name'      : <name>
+            'mobile'    : <mobile>
+            'email'     : <email>
+        }
+    returns OK
+    """
+    status, msg, json = utils.is_valid_user_info(bottle.request)
+    if status == 200:
+        utils.create_user(json)
+    return httpraise(status, msg)
---
+++ server/utils.py
@@ -0,0 +1,83 @@
+from pymongo import MongoClient
+
+
+client = MongoClient()
+
+
+def is_valid_login_request(request):
+    "Is this a valid login request?"
+    json = None
+    try:
+        json = request.json()
+    except:
+        return (422, 'JSON unreadable', None)
+    else:
+        if not all(i in json for i in ['email', 'pwd', 'token']):
+            return (422, 'email|pwd|token not in JSON', None)
+        if len(json['token']) != 100:
+            return (422, 'token len invalid', None)
+        if not is_token_available(json['token']):
+            return (422, 'regenerate token', None)
+        # OK
+        return (200, 'OK', json)
+
+
+def is_valid_logout_request(request):
+    "Is this a valid logout request?"
+    try:
+        json = request.json()
+    except:
+        return (422, 'JSON unreadable', None)
+    else:
+        if 'token' not in json:
+            return (422, 'token not in JSON', None)
+        if len(json['token']) != 100:
+            return (422, 'token len invalid', None)
+        if not is_token_available(json['token']):
+            return (422, 'invalid token', None)
+        # OK
+        return (200, 'OK', json)
+
+
+def is_valid_user_info(request):
+    "Is this valid info to create a new user?"
+    try:
+        json = request.json()
+    except:
+        return (422, 'JSON unreadable', None)
+    else:
+        if all(i not in json
+               for i in ['email', 'pwd', 'name',
+                         'address', 'mobile']):
+            return (422, 'keys missing in JSON', None)
+        # OK
+        return (200, 'OK', json)
+
+
+def is_token_available(token):
+    "Is the token available in the database?"
+    count = client.aang.tokens.find({'token': token}).count()
+    return count == 0
+
+
+def login_user(json):
+    "Create an entry in the token table"
+    email, pwd, token = json['email'], json['pwd'], json['token']
+    logid = {'email': email, 'pwd': pwd, 'token': token}
+    client.aang.tokens.insert_one(logid)
+
+
+def logout_user(json):
+    "Remove this entry from the token table"
+    token = json['token']
+    client.aang.tokens.find_one_and_delete({"token": token})
+
+
+def create_user(json):
+    "Create a user with this information"
+    data = dict(email=json['email'],
+                pwd=json['pwd'],
+                name=json['name'],
+                aadhaar=json['address'],
+                mobile=json['mobile'])
+    client.aang.users.insert_one(data)
