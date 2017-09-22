7290f5f63033b17cd2428a17035aee8a4a995aeb
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-09-22 23:10:37 +0530

    form creation API

--- server/server.py
+++ server/server.py
@@ -14,7 +14,7 @@ UPLOAD_DIR = 'uploads'  # NOTE: Needs to be replaced with amazon s3
 def httpraise(no, msg):
     """
     Helper function to easily set response status and return the needed
-    message
+    message on the outgoing response
     """
     bottle.response.status = no
     return msg
@@ -72,10 +72,10 @@ def user_create():
 
     Expects JSON
         {
-            'email'     : <identifier. Must be unique>
-            'address'   : <address>
-            'name'      : <name>
-            'mobile'    : <mobile>
+            'email'     : <identifier. Must be unique>,
+            'address'   : <address>,
+            'name'      : <name>,
+            'mobile'    : <mobile>,
             'pwd'       : <password hash>
         }
     returns OK
@@ -85,7 +85,7 @@ def user_create():
         utils.create_user(json)
     return httpraise(status, msg)
 
-# USER ROUTES #########################################
+# CONTENT ROUTES #########################################
 
 
 @app.post('/content/create')
@@ -130,6 +130,197 @@ def content_activate(contentid):
 def content_deactivate(contentid):
     # ADMIN
     pass
+
+
+# CONTENT ROUTES #########################################
+
+@app.post('/form/list')
+def form_list():
+    """
+    /form/list
+    Expects JSON
+        {
+            'token' : <logged in user token>
+        }
+
+    Returns JSON
+        {
+            'forms': [list of forms ids]
+        }
+    """
+    if utils.is_logged_in_user(bottle.request):
+        user = utils.get_user_from_token(bottle.request.json['token'])
+        if user is not None:
+            forms = utils.get_user_forms(user)
+            return {'forms': forms}
+        else:
+            return httpraise(404, 'no such user')
+    else:
+        return httpraise(403, 'login to access forms')
+    return httpraise(404, 'no forms found')
+
+
+@app.post('/form/<formid>')
+def form_formid(formid):
+    """
+    /form/<formid>
+    Expects JSON
+        {
+            'token': <logged in user token>,
+        }
+
+    Returns JSON
+        {
+            "formid"    : <formid>,
+            "title"     : <title>,
+            "fields"    :   [
+                                {
+                                    'label'     : <field label>,
+                                    'kind'      : <kind of field>,
+                                    'identifier': <identifier>
+                                },
+                                {
+                                    'label'     : <field label>,
+                                    'kind'      : <kind of field>,
+                                    'identifier': <identifier>
+                                }
+                            ]
+        }
+    """
+    if utils.is_logged_in_user(bottle.request):
+        ok = utils.user_is_allowed_form_access(bottle.request.json['token'],
+                                               formid)
+        if ok:
+            return utils.get_form(formid)
+        else:
+            return httpraise(403, 'Log in to access forms')
+    else:
+        return httpraise(403, 'Log in to access forms')
+
+
+@app.post('/form/create')
+def form_create():
+    """
+    /form/create
+
+    Expects JSON
+        {
+            'token' : <token of form creator>,
+            'form'  :   {
+                            'formid': <formid>,
+                            'title'     : <title>,
+                            'fields'    :   [
+                                                {
+                                                    'label'     :<field label>,
+                                                    'kind'      :<field kind>,
+                                                    'identifier':<identifier>
+                                                },
+                                                {
+                                                    'label'     :<field label>,
+                                                    'kind'      :<field kind>,
+                                                    'identifier':<identifier>
+                                                }
+                                            ],
+                            'groups'    : [group1, group2, ...]
+                        }
+
+    Returns JSON
+        {
+            'status': <true/false >,
+            'formid': <formid if created>
+        }
+    """
+    if utils.is_logged_in_user(bottle.request, admin=True):
+        ok, formid = utils.add_form_format(bottle.request.json)
+        if ok:
+            return {'status': True, 'formid': formid}
+        else:
+            return {'status': False, 'formid': None}
+    else:
+        return httpraise(403, 'admin access required')
+
+
+@app.post('/form/<formid>/submit')
+def form_submit(formid):
+    """
+    /form/<formid>/submit
+
+    Expects JSON
+        {
+            'token' : <user token>,
+            'data'  :   [
+                            {
+                                'identifier'    :<identifier>,
+                                'value'         :<value>
+                            },
+                            {
+                                'identifier'    :<identifier>,
+                                'value'         :<value>
+                            }
+                        ]
+        }
+
+    Returns JSON
+        {
+            'status'    : true
+        }
+    """
+    if utils.is_logged_in_user(bottle.request):
+        ok = utils.user_is_allowed_form_access(bottle.request.json['token'],
+                                               formid)
+        if ok:
+            status = utils.submit_form(formid, bottle.request.json['token'],
+                                       bottle.request.json['data'])
+            return {'status': status}
+        else:
+            return httpraise(403, 'Log in to access forms')
+    else:
+        return httpraise(403, 'login required')
+
+
+@app.post('/form/<formid>/deactivate')
+def form_deactivate(formid):
+    """
+    /form/<formid>/deactivate
+
+    Expects JSON
+        {
+            "token" : <user token>,
+        }
+
+    Returns JSON
+        {
+            "status": true/false
+        }
+    """
+    if utils.is_logged_in_user(bottle.request, admin=True):
+        status = utils.deactivate_form(formid)
+        return {'status': status}
+    else:
+        return httpraise(403, 'admin access required')
+
+
+@app.post('/form/<formid>/activate')
+def form_activate(formid):
+    """
+    /form/<formid>/activate
+
+    Expects JSON
+        {
+            'token': <user token>
+        }
+
+    Returns JSON
+
+        {
+            'status': true/false
+        }
+    """
+    if utils.is_logged_in_user(bottle.request, admin=True):
+        status = utils.activate_form(formid)
+        return {'status': status}
+    else:
+        return httpraise(403, 'admin access required')
 
 
 if __name__ == '__main__':
