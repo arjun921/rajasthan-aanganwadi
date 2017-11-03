87f7775541c8a82e57bf2fc09aeb3da8270c51e0
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-11-04 00:50:48 +0530

    debug logs for fname in server

--- server/server.py
+++ server/server.py
@@ -668,8 +668,10 @@ def form_delete():
 
 @app.get('/static/<link>')
 def staticfiles(link):
+    fname = None
+    print('before', link, fname, os.listdir(utils.upath), db.content_links)
     fname = db.content_get_fname_for_link(link)
-    print(fname, fname)
+    print('after', link, fname, os.listdir(utils.upath), db.content_links)
     if fname is not None:
         return bottle.static_file(fname, root=utils.upath)
     else:
