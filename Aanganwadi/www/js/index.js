8b8b24d92a37e759eb5ff93423cc41223fdabdea
Arjun S <arjun921@gmail.com>
2017-12-09 17:19:48 +0530

    navigation with file load in index complete

--- Aanganwadi/www/js/index.js
+++ Aanganwadi/www/js/index.js
@@ -12,16 +12,23 @@ function reINT() {
   //hides login/login based on cookie present/absent
   if (window.location.href.split('#').length==1) {
     createNav('_ROOT_');
-    $('#crumbtitle').innerHTML = "Home"
+    document.getElementById('crumbtitle').innerHTML = "Home";
   }
   else {
-    $('#crumbtitle').innerHTML = "Activity"
-    createNav(window.location.href.split('#')[1]);
+    document.getElementById('crumbtitle').innerHTML = "Activity";
+    if (location.hash.split(".").length<2) {
+      createNav(window.location.href.split('#')[1]);
+    } else {
+      console.log("Load content");
+      load_content(window.location.href.split('#')[1]);
+    }
+
   }
   loadSideMenu();
 }
 
 window.onpageshow = function(event) {
+
     reINT();
 };
 
@@ -30,56 +37,91 @@ window.onhashchange = change;
 
 //and read location.hash in the change function instead
 function change(){
+  $('#content').html('');
+  $('#contentCat').show();
+  $('#content').hide();
   reINT()
     var hash = "asda"
 }
 
 
+function load_content(contentID) {
+  $('#navi').html('');
+  $('#content').show();
+  $('#content').html('');
+  $.ajax({
+    url: (link + '/content'),
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      "token": Cookies.get('currenttoken'),
+      'fname': contentID
+    }),
+    success: function(data, st, xhr) {
+      data = data;
+      d = data;
+      ftype = (data.url.split('.').pop());
+
+      if (ftype == "mp4") {
+        p = "<video class=\"responsive-video\" style=\"width:100%; padding-top: 25px;\" controls><source src=" + link + data.url + " type=\"video/mp4\"></video>"
+        $('#content').append(p);
+        // p = "<div class=\"fixed-action-btn\" onclick=\"$('#content').html('');$('#contentCat').show();$('#content').hide();\"><a class=\"btn-floating btn-large red\" ><i class=\"large material-icons\">arrow_back</i></a></div>"
+        // $('#content').append(p);
+      } else if (ftype == "mp3"){
+        p = "<p>"+contentID+"</p><audio controls=\"controls\" style=\"width:100%; padding-top: 25px;\" id = \"player\"><source src = " + link + data.url + " /></audio>"
+        $('#content').append(p);
+        // p = "<div class=\"fixed-action-btn\" onclick=\"$('#content').html('');$('#contentCat').show();$('#content').hide();\"><a class=\"btn-floating btn-large red\" ><i class=\"large material-icons\">arrow_back</i></a></div>"
+        // $('#content').append(p);
+      } else if (ftype == "pdf") {
+        // "https://docs.google.com/viewer?srcid=[YOUR_FILE'S_ID_HERE]&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+        flink = 'https://docs.google.com/viewer?url=' + link + data.url+"&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+        console.log(flink);
+        p = "<iframe src=\""+flink+"\" style=\"position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\">Your browser doesn't support iframes</iframe>"
+        // window.open('https://docs.google.com/viewer?url=' + link + data.url, '_self', 'location=yes');
+        $('#content').append(p);
+      }
+
+    },
+    error: function(returnval) {
+      if (returnval.status != 200) {
+        out_changes();
+        var $toastContent = $('<span>Please Login to view.</span>').add($('<a href="login.html"><button class="btn-flat toast-action">OK</button></a>'));
+        Materialize.toast($toastContent, 4000, '', function() {
+          window.open("login.html", "_self")
+        })
+      }
+
+    }
+  });
+  //load form based on id requested
+  // return create
+}
+
+
 function createNav(id) {
   $('#navi').html('');
-
     $.ajax({
         url: (link + '/category'),
         type: 'post',
         contentType: 'application/json',
         data: JSON.stringify({'catid': id}),
         success: function(data, st, xhr) {
-          if ($.inArray(data.id, old_id)==-1) {
-            old_id.push(data.id);
-          }
-          var files = [];
-          for (var i = 0; i < data.contains.length; i++) {
-            cont = data.contains[i];
-            if (cont.id[0]!='_') {
-              ftype = cont.id.split('.').pop();
-              file = {}
-              file[ftype] = [data.contains[i]];
-              file['type'] = ftype;
-              files.push(file)
-                Cookies.set('mediaCont', true);
-                Cookies.set('media', { files});
+              for (var i = 0; i < data.contains.length; i++) {
+                item = data.contains[i];
+                console.log(item);
+                p = "<a class=\"collection-item\" onclick=\"navClick(this.id)\" id=\""+item.id+"\">"+item.title+"</a>";
+                $('#navi').append(p);
               }
             }
-          if (Cookies.get('mediaCont')) {
-            s = Cookies.get('media');
-            s = JSON.parse(s);
-            Cookies.remove('mediaCont');
-            window.open('content.html', '_self', 'location=yes');
-          }
-          else {
-            for (var i = 0; i < data.contains.length; i++) {
-              item = data.contains[i];
-              p = "<a class=\"collection-item\" onclick=\"navClick(this.id)\" id=\""+item.id+"\">"+item.title+"</a>";
-              $('#navi').append(p);
-            }
-          }
-        }
       });
 
 }
 
 
 function navClick(id) {
+
+  // console.log(id.split('.')[id.split('.').length-1]);
+
   $('#navi').html('');
   url = window.location.href.split('#')[0]+"#"+id;
   window.location.href = url;
--- UI/js/index.js
+++ UI/js/index.js
@@ -1,7 +1,13 @@
 var lastElem = "form";
 var formslist = [];
 var data;
-var old_id = [];
+
+window.onpageshow = function(event) {
+    reINT();
+};
+
+
+window.onhashchange = change;
 
 function reINT() {
   $("#profile_pic").hide();
@@ -16,28 +22,83 @@ function reINT() {
   }
   else {
     document.getElementById('crumbtitle').innerHTML = "Activity";
-    createNav(window.location.href.split('#')[1]);
+    if (location.hash.split(".").length<2) {
+      createNav(window.location.href.split('#')[1]);
+    } else {
+      load_content(window.location.href.split('#')[1]);
+    }
+
   }
   loadSideMenu();
 }
 
-window.onpageshow = function(event) {
-    reINT();
-};
 
-
-window.onhashchange = change;
 
 //and read location.hash in the change function instead
 function change(){
+  $('#content').html('');
+  $('#contentCat').show();
+  $('#content').hide();
   reINT()
     var hash = "asda"
 }
 
 
+function load_content(contentID) {
+  $('#navi').html('');
+  $('#content').show();
+  $('#content').html('');
+  $.ajax({
+    url: (link + '/content'),
+    type: 'post',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      "token": Cookies.get('currenttoken'),
+      'fname': contentID
+    }),
+    success: function(data, st, xhr) {
+      data = data;
+      d = data;
+      ftype = (data.url.split('.').pop());
+
+      if (ftype == "mp4") {
+        p = "<video class=\"responsive-video\" style=\"width:100%; padding-top: 25px;\" controls><source src=" + link + data.url + " type=\"video/mp4\"></video>"
+        $('#content').append(p);
+        // p = "<div class=\"fixed-action-btn\" onclick=\"$('#content').html('');$('#contentCat').show();$('#content').hide();\"><a class=\"btn-floating btn-large red\" ><i class=\"large material-icons\">arrow_back</i></a></div>"
+        // $('#content').append(p);
+      } else if (ftype == "mp3"){
+        p = "<p>"+contentID+"</p><audio controls=\"controls\" style=\"width:100%; padding-top: 25px;\" id = \"player\"><source src = " + link + data.url + " /></audio>"
+        $('#content').append(p);
+        // p = "<div class=\"fixed-action-btn\" onclick=\"$('#content').html('');$('#contentCat').show();$('#content').hide();\"><a class=\"btn-floating btn-large red\" ><i class=\"large material-icons\">arrow_back</i></a></div>"
+        // $('#content').append(p);
+      } else if (ftype == "pdf") {
+        // "https://docs.google.com/viewer?srcid=[YOUR_FILE'S_ID_HERE]&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+        flink = 'https://docs.google.com/viewer?url=' + link + data.url+"&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+
+        p = "<iframe src=\""+flink+"\" style=\"position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\">Your browser doesn't support iframes</iframe>"
+        // window.open('https://docs.google.com/viewer?url=' + link + data.url, '_self', 'location=yes');
+        $('#content').append(p);
+      }
+
+    },
+    error: function(returnval) {
+      if (returnval.status != 200) {
+        out_changes();
+        var $toastContent = $('<span>Please Login to view.</span>').add($('<a href="../UI/login.html"><button class="btn-flat toast-action">OK</button></a>'));
+        Materialize.toast($toastContent, 4000, '', function() {
+          window.open("../UI/login.html", "_self")
+        })
+      }
+
+    }
+  });
+  //load form based on id requested
+  // return create
+}
+
+
 function createNav(id) {
   $('#navi').html('');
-
     $.ajax({
         url: (link + '/category'),
         type: 'post',
@@ -46,7 +107,6 @@ function createNav(id) {
         success: function(data, st, xhr) {
               for (var i = 0; i < data.contains.length; i++) {
                 item = data.contains[i];
-                console.log(item);
                 p = "<a class=\"collection-item\" onclick=\"navClick(this.id)\" id=\""+item.id+"\">"+item.title+"</a>";
                 $('#navi').append(p);
               }
@@ -57,7 +117,6 @@ function createNav(id) {
 
 
 function navClick(id) {
-  console.log(document.getElementById(id).innerHTML);
   $('#navi').html('');
   url = window.location.href.split('#')[0]+"#"+id;
   window.location.href = url;
