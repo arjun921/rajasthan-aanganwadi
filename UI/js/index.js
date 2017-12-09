1795b75617e7410f898809a305d07246b3819e07
Arjun S <arjun921@gmail.com>
2017-12-09 11:52:18 +0530

    Added common.js

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,17 +1,8 @@
-var link = 'https://rajasthan-aanganwadi.herokuapp.com';
-// var link = 'http://192.168.225.163:8000';
 var lastElem = "form";
 var formslist = [];
 var data;
 var old_id = [];
 
-$( document ).ajaxStart(function() {
-  NProgress.start();
-});
-
-$(document).ajaxSuccess(function() {
-  NProgress.done();
-});
 // $( ".trigger" ).click(function() {
 //   console.log("Ajax End");
 // });
@@ -84,41 +75,6 @@ function navClick(id) {
   $("#_ROOT_").hide();
 }
 
-function loadSideMenu() {
-  $.ajax({
-      url: (link + '/category'),
-      type: 'post',
-      contentType: 'application/json',
-      data: JSON.stringify({'catid': '_ROOT_'}),
-      success: function(data, st, xhr) {
-        for (var i = 0; i < data.contains.length; i++) {
-          item = data.contains[i];
-          // s = "<li><a class=\"dropdown-button\" onclick=\"$('.button-collapse').sideNav('hide');navClick(this.id)\" id=\""+item.id+"\">"+item.title+"</a></li>"
-          s = "<li><a class=\"dropdown-button\" href=\"index.html#"+item.id+"\" onclick=\"$('.button-collapse').sideNav('hide');\" id=\"" + item.id + "\">" + item.title + "</a></li>"
-          $('#mobile-demo').append(s);
-
-        }
-        s = "<li><div class=\"divider\"></div></li><li><a class=\"waves-effect \" href=\"index.html\"><i class=\"material-icons\">home</i>Home</a></li><li><a class=\"waves-effect \" href=\"#!\"><i class=\"material-icons\">settings</i>Settings</a></li><li><a class=\"waves-effect\" href=\"all_forms.html\"><i class=\"material-icons\">format_align_left</i>Forms to Fill</a></li><li><a class=\"waves-effect\" href=\"#!\" onclick=\"logout()\" id=\"logout_menu_but\"><i class=\"material-icons\">exit_to_app</i>Logout</a></li><li><a class=\"waves-effect\" href=\"login.html\" id=\"login_menu_but\"><i class=\"material-icons\">exit_to_app</i>Login</a></li>"
-        $('#mobile-demo').append(s);
-        $("#loggedIn").show();
-        $("#noLogin").hide();
-        //sets navigation menu profile content
-        if (Cookies.get('currenttoken')) {
-          $("#email_menu").text(Cookies.get('email'));
-          $("#name_menu").text("Arjoonn Sharma");
-          $("#profile_pic").show();
-          $("#profile_pic").attr('src',"images/turban22.png");
-          $("#login_menu_but").hide();
-        }
-        else {
-          out_changes();
-        }
-      NProgress.set(1.0);
-      }
-
-    });
-}
-
 function reINT() {
   $("#profile_pic").hide();
   $('select').material_select();
@@ -135,35 +91,3 @@ function reINT() {
 $(document).ready(function() {
   reINT();
 });
-
-function out_changes() {
-  $("#profile_pic").show();
-  $("#profile_pic").attr('src',"images/empty-profile.gif");
-  $("#loggedIn").hide();
-  $("#logout_menu_but").hide();
-  $("#login_menu_but").show();
-  $("#name_menu").text(" ");
-  $("#noLogin").show();
-  Cookies.remove('currenttoken');
-  Cookies.remove('email');
-}
-
-function logout() {
-  $.ajax({
-    url: (link + '/user/logout'),
-    type: 'post',
-    contentType: 'application/json',
-    data: JSON.stringify({
-      "token": Cookies.get('currenttoken')
-    }),
-    success: function(data, st, xhr) {
-      if (xhr.status == 200) {
-        Materialize.toast('User Logout Successful', 4000,'',function(){window.open("../UI/index.html","_self")})
-      }
-    },
-    error: function(returnval) {
-      NProgress.done();
-    }
-  });
-  out_changes();
-}
