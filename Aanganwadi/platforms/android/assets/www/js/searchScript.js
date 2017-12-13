2e5198c26606756e3665a036ce91fac255fc6185
Arjun S <arjun921@gmail.com>
2017-12-13 15:35:59 +0530

    Pretty Pagination added

--- Aanganwadi/platforms/android/assets/www/js/searchScript.js
+++ Aanganwadi/platforms/android/assets/www/js/searchScript.js
@@ -53,9 +53,14 @@ var updateCategoriesTable = function(Categories) {
 
   var tokens = search.tokenizer.tokenize(searchInput.value);
   document.getElementById('navi').innerHTML = '';
+  document.getElementById('pagination').innerHTML = '';
+  var c = count+1;
+
   if (totalCategories>paginateSplit && start>=paginateSplit) {
-    p = "<a class=\"collection-item\" onclick=\"loadPreviousList50()\" id=\""+item.id+"\">Previous..</a>";
-    $('#navi').append(p);
+    // p = "<p class=\"center\">Page: "+c+"</p>"
+    // $('#navi').append(p);
+    p = "<li class=\"waves-effect\" onclick=\"loadPreviousList50()\"><a><i class=\"material-icons\">chevron_left</i></a></li>";
+    $('#pagination').append(p);
   }
   for (var i = start, length = end; i < length; i++) {
     item = Categories[i];
@@ -63,8 +68,14 @@ var updateCategoriesTable = function(Categories) {
     p = "<a class=\"collection-item\" onclick=\"navClick(this.id)\" id=\""+item.id+"\">"+item.title+"</a>";
     $('#navi').append(p);
   }
+
   if (Categories.length>end) {
-    p = "<a class=\"collection-item\" onclick=\"loadNextList50()\" id=\""+item.id+"\">More..</a>";
-    $('#navi').append(p);
+    //conditionally show next button
+    p="<li class=\"center\"><a id=\"pageNo\">Page: "+c+"</a></li>"
+    $('#pagination').append(p);
+    p = "<li class=\"waves-effect\" onclick=\"loadNextList50()\"><a><i class=\"material-icons\">chevron_right</i></a></li>";
+    $('#pagination').append(p);
+
   }
+
 };
