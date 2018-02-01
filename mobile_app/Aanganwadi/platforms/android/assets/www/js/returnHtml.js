2dab52cf9101a4e97f9aa6e7982dabcdc96f67c6
Arjun S <arjun921@gmail.com>
2018-02-02 02:10:24 +0530

    Reorganized app content

---
+++ mobile_app/Aanganwadi/platforms/android/assets/www/js/returnHtml.js
@@ -0,0 +1,82 @@
+// All HTML Strings go here.
+// Created for improving code
+function getHTMLTxtField(field) {
+  return "<div class=\"input-field col s6\"><input id=" + field.id + " type=\"text\" " + "><label for=" + field.id + ">" + field.label + "</label></div>"
+}
+
+function getHTMLLabel(field) {
+  return "<p>" + field.label + "</p>"
+}
+
+function getHTMLRadioButtons(field,va) {
+  return "<p>  <input name=" + field.id + " type=\"radio\" id=" + va.subID + " value=" + va.subID + " />  <label for=" + va.subID + ">" + va.subLabel + "</label></p>"
+}
+
+function getHTMLCheckBoxes(cbv) {
+  return "<p><input type=\"checkbox\" id=" + cbv.subID + " /><label for=" + cbv.subID + ">" + cbv.subLabel + "</label></p>"
+}
+
+function getHTMLSelect(field) {
+  return "<select id=" + field.id + "><option value=\"\" disabled selected>Choose your option</option></select>"
+}
+
+function getHTMLSelectFields(vs) {
+  return "<option value=" + vs.subVal + ">" + vs.subLabel + "</option>"
+}
+
+function getHTMLRange(field) {
+  return "<p class=\"range-field\"><input type=\"range\" id=" + field.id + " min=" + field.misc[0].min + " max=" + field.misc[0].max + " /></p>"
+}
+
+function getHTMLDatePicker(field) {
+  return "<input type=\"text\" class=\"datepicker\" id=" + field.id + " placeholder=\"Choose Date\">"
+}
+
+function getHTMLTimePicker(field) {
+  return "<input type=\"text\" class=\"timepicker\" id=" + field.id + " placeholder=\"Select Time\">"
+}
+
+function getHTMLFormsListElement(item) {
+  return "<a class=\"collection-item\" onclick=\"load_form(this.id)\" id=\""+item.formid+"\">"+item.title+"</a>";
+}
+
+function getHTMLNoFormsToFill() {
+  return "<a class=\"collection-item\" href=\"index.html\">No Forms to fill at the moment</a>"
+}
+
+function getHTMLFormSubmitButton() {
+  return "<button style=\"padding-bottom:20px;\" class=\"btn waves-effect waves-light\"onclick=\"doSubmit()\">Submit<i class=\"material-icons right\">send</i></button>"
+}
+
+function getHTMLFormTitle(fields_returned) {
+  return "<h5>" + fields_returned.title + "</h5>"
+}
+
+function getHTMLVideoPlayer(data) {
+  return "<video class=\"responsive-video\" style=\"width:100%; padding-top: 25px;\" controls controlsList=\"nodownload\"><source src=" + server + data.url + " type=\"video/mp4\"></video>"
+}
+
+function getHTMLAudioPlayer(data) {
+  return "<p></p><audio  controlsList=\"nodownload\" controls=\"controls\" style=\"width:100%; padding-top: 25px;\" id = \"player\"><source src = " + server + data.url + " /></audio>"
+}
+
+function getHTMLPDFViewer(flink) {
+  return "<iframe onload=\"$('#closeIcon').addClass('black-text');$('#closeTabletIcon').addClass('black-text');$('#CloseTablet').removeClass('tabletClose');$('#CloseTablet').addClass('tabletClosePdf');$('#preloader').hide();\" src=\""+flink+"\" class=\"z-depth-4\" id=\"docIframe\" style=\"position:absolute; top:0px; left:0px; bottom:0px; z-index: 2; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;\">Your browser doesn't support iframes</iframe>"
+}
+
+function getHTMLCategoryFileListElement(item){
+  fileType = getFileType(item);
+  strBegin = " <li class=\"collection-item\" id=\""+item.id+"\" onclick=\"navClick(this.id)\"><div>"+item.title+"<div class=\"secondary-content \"><i class=\"material-icons\">"
+  icon = getIcon(fileType);
+  strEnd = "</i></div></li>"
+  p = strBegin+icon+strEnd
+  return p
+}
+
+function getHTMLCategoryUp(){
+  strBegin = " <li class=\"collection-item  \" onclick=\"window.history.go(-1);\"><div>Go Up...<div class=\"secondary-content center\" >"
+  icon = "<i class=\"material-icons \">chevron_left"
+  strEnd = "</i></div></li>"
+  p = strBegin+icon+strEnd
+  return p
+}
---
+++ mobile_app/README.md
@@ -0,0 +1,63 @@
+## Aanganwadi App
+
+### Folder structure
+
+- `Aanganwadi ` folder is the phonegap project folder. 
+- `Aanganwadi/www` contains the html files for the app.
+
+### Running the app using phonegap.
+
+Make sure you `phonegap cli` installed.  Refer [link](http://docs.phonegap.com/getting-started/1-install-phonegap/cli/) for more information
+
+Make sure to have all the dependencies for each platform installed that you're trying to run.
+
+First navigate to the phonegap project folder.
+
+`$ cd Aanganwadi`
+
+#### Android
+
+Connect device/emulator to adb.
+
+`$ phonegap run android`
+
+Disclaimer: Needs Android SDK, adb tools and adb drivers to be installed and adb access granted if running from device. 
+
+#### iOS
+
+`$ phonegap run ios`
+
+Disclaimer: Needs Xcode and xcode developer tools installed on a MacOS machine with phonegap to run iOS app on local tethered device. 
+
+#### Windows
+
+Depending on platform of interest
+
+`$ phonegap run wp8` / `$ phonegap run windows` 
+
+Disclaimer: needs visual studio and other prequisites installed on a windows machine to build for windows phone.
+
+### Creating app for deployment
+
+##### Android
+
+In order to create a keystore with a different password:
+
+`keytool -genkey -v -keystore Aanganwadi.keystore -alias Aanganwadi -keyalg RSA -keysize 2048 -validity 100000`
+
+To create app run the following
+
+`$ cd Aanganwadi && phonegap build android --release -- --keystore="../key/Aanganwadi.keystore"  --storePassword=aanganwadi --alias=aanganwadi`
+
+Enter password "aanganwadi"
+
+`$ open platforms/android/build/outputs/apk`
+
+**iOS**
+
+Due to non availability of iOS developer licence, a guide hasn't been mentioned. For information on deployment of phonegap app to iOS devices, refer the following [link](https://stackoverflow.com/questions/6632350/how-to-deploy-phonegap-app-to-ipad-iphone). 
+
+**Windows**
+
+Process of setting up and building for windows isn't small enough to include in readme, hence please refer  [official documentation](http://docs.phonegap.com/en/edge/guide_platforms_wp8_index.md.html) on how to deploy to windows devices.
+
